a784fabdbd058018dea909cca345b449eb992074
KanchanVerma <69891196+nooneknows199@users.noreply.github.com>
2021-07-04 15:40:26 +0530

    Update Day-19: SubarrayWithSumEqualToZero.cpp

--- Day-19: SubarrayWithSumEqualToZero.cpp
+++ Day-19: SubarrayWithSumEqualToZero.cpp
@@ -1,3 +1,38 @@
+#include <iostream>    // T.C : O(n*n)
+#include <unordered_map>
+using namespace std;
+ 
+void printAllSubarrays(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        for (int j = i; j < n; j++)
+        {
+            sum += arr[j];
+            if (sum == 0) {
+                cout << "Subarray [" << i << "…" << j << "]\n";
+            }
+        }
+    }
+}
+ 
+int main()
+{
+    #ifndef ONLINE_JUDGE
+  freopen("input.txt","r",stdin);
+  freopen("output.txt","w",stdout);
+  #endif
+
+    int arr[] = { 4, -4, -3, -1, 0, 4 };
+    int n = sizeof(arr)/sizeof(arr[0]);
+ 
+    printAllSubarrays(arr, n);
+ 
+    return 0;
+}
+
+// Method 02:
 #include <bits/stdc++.h>        // T.C : O(n)
 using namespace std;            // S.C :(n)
  
